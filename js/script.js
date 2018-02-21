82282b706540a1d7eadf1fb4b5f19b41d66bc3ee
camposveronica <36063615+camposveronica@users.noreply.github.com>
2018-02-20 17:39:19 -0800

    Assignment_01
    Assignment_01

---
+++ js/script.js
@@ -0,0 +1,134 @@
+/*eslint-env browser*/
+
+//STEP 1
+/*someMonth
+theMonth
+currentMonth
+summerMonth
+myLibraryAFunction*/
+
+//STEP 2
+/*3
+"Some word"
+true
+null*/
+
+//STEP 3
+/*
+var firstExample = 6 * (20 / 4) - 2;
+var example2 = 5 + 5;
+*/
+
+//STEP 4
+/*var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var strZipCode;
+var intYourAge;
+var strReferralSource;
+var blnMayWeContactYou;*/
+
+//STEP 5
+/*var strFirstName;
+strFirstName = "Zak";
+var strLastName = "Ruvalcaba";
+var strAddress = "555 Sample Dr.", strCity = "San Diego";*/
+
+//STEP 6
+/*var nameAndAge = "Zak Ruvalcaba " + 27;
+var zakIsInstructor = "Is Zak Ruvalcaba the Instructor? " + true;
+var ageIsInstructor = 27 + true;
+window.console.log(nameAndAge);
+window.console.log(zakIsInstructor);
+window.console.log(ageIsInstructor);*/
+
+//STEP 7
+//var car = "Chevy";
+//window.console.log("My " + car + " " + model + " is at the shop");
+//var model = "Camaro";
+
+//STEP 8
+/*var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);*/
+
+//STEP 9
+/*var x = null;
+window.console.log(x);
+var y;
+window.console.log(y);*/
+
+//STEP 10
+/*window.console.log(typeof "Zak Ruvalcaba");
+window.console.log(typeof 5.32);
+window.console.log(typeof true);
+window.console.log(typeof {name: "Zak Ruvalcaba"});
+window.console.log(typeof myHouse);*/
+
+//STEP 11
+/*window.alert("Hello " + "Zak Ruvalcaba" + ", welcome to the JavaScript class!");*/
+
+//STEP 12
+/*var name = "Zak Ruvalcaba";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");*/
+
+//STEP 13
+/*var name = "Zak Ruvalcaba";
+var course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");*/
+
+//STEP 14
+/*var name = "Zak Ruvalcaba";
+var course = "JavaScript";
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+//STEP 15
+/*var name = window.prompt("What is your name?");
+var course = "JavaScript";
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+//STEP 16
+/*var name = window.prompt("What is your name?");
+var course = window.prompt("What is the course name?");
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+//STEP 17
+/*var x = 10;
+var y = 20;
+window.console.log(x + y);*/
+
+//STEP 18
+/*var x = 20;
+x += 20;
+window.console.log(x);*/
+
+//STEP 19
+/*var x = 20;
+x *= 5;
+window.console.log(x);*/
+
+//STEP 20
+/*var x = 20 % 3;
+x /= 1;
+window.console.log(x);*/
+
+//STEP 21
+/*var x = (5 > 4) || (7 > 10);
+window.console.log(x);*/
+
+//STEP 22
+/*var x = (5 > 4) && (7 > 10);
+window.console.log(x);*/
+
+//STEP 23
+/*var widget = new Object();
+window.console.log(typeof widget);*/
+
+//STEP 24
+/*var widget = {};
+window.console.log(widget instanceof Object);*/
+
+//STEP 25
+/*var widget = {};
+window.console.log(widget instanceof Date);*/
